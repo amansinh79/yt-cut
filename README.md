d28ae3f53193b1ddf519985ad2feb3e26adc01a5
solvencino <harshparamar84@gmail.com>
2022-03-03 21:46:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+`yc <url> -s <start_time> -e <end_time> -o <filename.mp4>`
+
+times are required to enter in hh:mm:ss format.
+
+output is optional. defaults to output.mp4
+
+depends on ffmpeg.
---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+const minimist = require('minimist')
+const ytdl = require('ytdl-core')
+const { spawn } = require('child_process')
+const inq = require('inquirer')
+
+const args = minimist(process.argv.slice(2))
+const start = args.s && hmsToSecondsOnly(args.s.toString())
+const end = args.e && hmsToSecondsOnly(args.e.toString())
+const url = args._[0]
+const op = args.o || 'output.mp4'
+console.log('Loading...')
+
+ytdl.getInfo(url).then(async (info) => {
+  const length = info.videoDetails.lengthSeconds - 1
+
+  if (end && end > length) {
+    console.log('End out of range!')
+    process.exit()
+  }
+
+  const videoStreams = info.formats.filter((t) => t.hasVideo && !t.hasAudio && t.container === 'mp4')
+  const audioStreams = info.formats.filter((t) => !t.hasVideo && t.hasAudio && t.container === 'mp4')
+
+  let { video } = await inq.prompt({
+    name: 'video',
+    type: 'list',
+    message: 'choose video quality',
+    choices: videoStreams.map((t) => t.qualityLabel + ' ' + t.videoCodec),
+    loop: false,
+  })
+
+  video = videoStreams.find((t) => t.qualityLabel + ' ' + t.videoCodec === video).url
+
+  let { audio } = await inq.prompt({
+    name: 'audio',
+    type: 'list',
+    message: 'choose audio quality',
+    choices: audioStreams.map((t) => t.audioQuality + ' ' + t.container),
+    loop: false,
+  })
+
+  audio = audioStreams.find((t) => t.audioQuality + ' ' + t.container === audio).url
+
+  const ss = start ? ['-ss', start] : []
+  const e = end ? ['-t', end - start] : []
+
+  const proc = spawn('ffmpeg', ['-hide_banner', '-y', ...ss, '-i', video, ...ss, '-i', audio, '-c:v', 'copy', '-c:a', 'copy', ...e, op])
+  proc.stderr.pipe(process.stderr)
+  proc.stdout.pipe(process.stdout)
+})
+
+function hmsToSecondsOnly(str) {
+  var p = str.split(':'),
+    s = 0,
+    m = 1
+
+  while (p.length > 0) {
+    s += m * parseInt(p.pop(), 10)
+    m *= 60
+  }
+
+  return s
+}

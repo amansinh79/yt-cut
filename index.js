acbfe3798e5aa8f5a0a9b6d1c7b9fb74ebfa4207
Amansinh Parmar <harshparamar84@gmail.com>
2022-03-07 18:44:07 +0530

    add audio only

--- index.js
+++ index.js
@@ -1,68 +1,111 @@
 #!/usr/bin/env node
 
-const minimist = require('minimist')
-const ytdl = require('ytdl-core')
-const { spawn } = require('child_process')
-const inq = require('inquirer')
+const minimist = require("minimist");
+const ytdl = require("ytdl-core");
+const { spawn } = require("child_process");
+const inq = require("inquirer");
 
-const args = minimist(process.argv.slice(2))
-const start = args.s && hmsToSecondsOnly(args.s.toString())
-const end = args.e && hmsToSecondsOnly(args.e.toString())
-const url = args._[0]
-const op = args.o || 'output.mp4'
-const r = args.r
+const args = minimist(process.argv.slice(2));
+const start = args.s && hmsToSecondsOnly(args.s.toString());
+const end = args.e && hmsToSecondsOnly(args.e.toString());
+const url = args._[0];
+let op = args.o;
+const r = args.r;
+const a = args.a;
 
-console.log('Loading...')
+console.log("Loading...");
 
 ytdl.getInfo(url).then(async (info) => {
-  const length = info.videoDetails.lengthSeconds - 1
+  const length = info.videoDetails.lengthSeconds - 1;
 
   if (end && end > length) {
-    console.log('End out of range!')
-    process.exit()
+    console.log("End out of range!");
+    process.exit();
   }
 
-  const videoStreams = info.formats.filter((t) => t.hasVideo && !t.hasAudio)
-  const audioStreams = info.formats.filter((t) => !t.hasVideo && t.hasAudio)
+  if (!op) {
+    op = info.videoDetails.title;
+  }
 
-  let { video } = await inq.prompt({
-    name: 'video',
-    type: 'list',
-    message: 'choose video quality',
-    choices: videoStreams.map((t) => t.qualityLabel + ' ' + t.videoCodec + ' ' + t.container),
-    loop: false,
-  })
+  const videoStreams = info.formats.filter((t) => t.hasVideo && !t.hasAudio);
+  const audioStreams = info.formats.filter((t) => !t.hasVideo && t.hasAudio);
 
-  video = videoStreams.find((t) => t.qualityLabel + ' ' + t.videoCodec + ' ' + t.container === video).url
+  let video;
+  if (!a) {
+    video = (
+      await inq.prompt({
+        name: "video",
+        type: "list",
+        message: "choose video quality",
+        choices: videoStreams.map(
+          (t) => t.qualityLabel + " " + t.videoCodec + " " + t.container
+        ),
+        loop: false,
+      })
+    ).video;
+
+    video = videoStreams.find(
+      (t) => t.qualityLabel + " " + t.videoCodec + " " + t.container === video
+    ).url;
+  }
 
   let { audio } = await inq.prompt({
-    name: 'audio',
-    type: 'list',
-    message: 'choose audio quality',
-    choices: audioStreams.map((t) => t.audioQuality + ' ' + t.container),
+    name: "audio",
+    type: "list",
+    message: "choose audio quality",
+    choices: audioStreams.map((t) => t.audioQuality + " " + t.container),
     loop: false,
-  })
+  });
 
-  audio = audioStreams.find((t) => t.audioQuality + ' ' + t.container === audio).url
+  audio = audioStreams.find(
+    (t) => t.audioQuality + " " + t.container === audio
+  ).url;
 
-  const ss = start ? ['-ss', start] : []
-  const e = end ? ['-t', end - start] : []
-  const re_enc = r ? [] : ['-c:v', 'copy', '-c:a', 'copy']
+  const ss = start ? ["-ss", start] : [];
+  const e = end ? ["-t", end - start] : [];
+  const re_enc = r ? [] : ["-c:v", "copy", "-c:a", "copy"];
 
-  const proc = spawn('ffmpeg', ['-hide_banner', '-y', ...ss, '-i', video, ...ss, '-i', audio, ...re_enc, ...e, op])
-  proc.stderr.pipe(process.stderr)
-  proc.stdout.pipe(process.stdout)
-})
+  let proc;
+  if (!a) {
+    proc = spawn("ffmpeg", [
+      "-hide_banner",
+      "-y",
+      ...ss,
+      "-i",
+      video,
+      ...ss,
+      "-i",
+      audio,
+      ...re_enc,
+      ...e,
+      op + ".mp4",
+    ]);
+  } else {
+    proc = spawn("ffmpeg", [
+      "-hide_banner",
+      "-y",
+      ...ss,
+      "-i",
+      audio,
+      "-b:a",
+      "320k",
+      ...e,
+      op + ".mp3",
+    ]);
+  }
+  proc.stderr.pipe(process.stderr);
+  proc.stdout.pipe(process.stdout);
+});
 
 function hmsToSecondsOnly(str) {
-  var p = str.split(':'),
+  var p = str.split(":"),
     s = 0,
-    m = 1
+    m = 1;
 
   while (p.length > 0) {
-    s += m * parseInt(p.pop(), 10)
-    m *= 60
+    s += m * parseInt(p.pop(), 10);
+    m *= 60;
   }
 
-  return s
+  return s;
 }
